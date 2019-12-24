ad1133b541002b7c70425b17915b45eae52fd1f3
alpaca-tc <alpaca-tc@alpaca.tc>
2019-12-25 03:24:51 +0900

    Remove unused class

--- lib/active_record_bitmask.rb
+++ lib/active_record_bitmask.rb
@@ -3,7 +3,6 @@
 module ActiveRecordBitmask
   require 'active_record_bitmask/version'
   require 'active_record_bitmask/attribute_methods'
-  require 'active_record_bitmask/bitmask'
   require 'active_record_bitmask/definition'
   require 'active_record_bitmask/bitmask_type'
   require 'active_record_bitmask/map'
